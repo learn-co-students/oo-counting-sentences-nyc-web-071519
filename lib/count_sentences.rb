f2ce80c402a0903f9babdbe2887ec9d123be724b
Luis Ascencio <luismiguelascencio@gmail.com>
2019-07-17 05:06:15 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,41 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+
   end
 
   def question?
-
+self.end_with?("?")
   end
 
   def exclamation?
-
+self.end_with?("!")
   end
 
   def count_sentences
+if self==""
+  return 0
 
+elsif self.split(" ").count>3
+
+  self.scan(/[^\.!?]+[\.!?]/).count
+  #  x=self.split(".")
+  #  x.join(" ")
+  
+  
+  #binding.pry
+
+else self.split(".")==3
+self.split(".").count
+
+#else
+  #return "test"
+
+#else self.split(".")>3
+  
+
+
+end
   end
 end
